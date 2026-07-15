9d6cab5683e208c685013d0c028406317a53bbd2
ariki-tomono <ariki@tomono.co.jp>
2026-07-15 09:14:16 +0900

    GitHub Pagesでの公開完了

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/aws-cpe-study-app/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
